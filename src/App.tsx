636bff803801d9c5b8e90532031fb21957c79b7e
mahnoor <faisalmahnoor74@gmail.com>
2024-01-04 20:54:16 +0500

    pause resume btn

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,21 @@ function App() {
   
   //.............................2nd method........................................................................
   const [count, setCount] = useState<number>(0);
+  const [paused, setPaused] = useState<string>("resume");
   useEffect(() => {
    const intervalId=setInterval(() => {
+    if(paused==="resume")
      setCount(count=>count+1);
     }, 1000); 
-    
     return () => clearInterval(intervalId);
-  },[]);
+  },[paused]);
 
   return (
     <>
-    <p>count is {count}</p>
-    <button onClick={() => setCount(0)}>Reset</button>
+    <h1 className="heading">Count is {count}</h1>
+    <button id="resetbtn" onClick={() => setCount(0)}>Reset</button>
+    <button id="resetbtn" onClick={() =>setPaused("pause")}>Pause</button>
+    <button id="resetbtn" onClick={() =>setPaused("resume")}>Resume</button>
     </>
     )
 }
