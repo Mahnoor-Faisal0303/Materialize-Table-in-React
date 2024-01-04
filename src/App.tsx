ec811d2358438fd32528625d8e3eb6a694aefb38
mahnoor <faisalmahnoor74@gmail.com>
2024-01-04 18:45:21 +0500

    2nd

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-       setTimeout(() => {
-        setCount(count+1);
-      }, 1000);  
+  // const [count, setCount] = useState<number>(0)
+  //      setTimeout(() => {
+  //       setCount(count+1);
+  //     }, 1000); 
+  
+  
+  //.............................2nd method........................................................................
+  const [count, setCount] = useState<number>(0);
+  useEffect(() => {
+   const intervalId=setInterval(() => {
+     setCount(count=>count+1);
+    }, 1000); 
+    return () => clearInterval(intervalId);
+  },[]);
 
   return (
     <p>count is {count}</p>
