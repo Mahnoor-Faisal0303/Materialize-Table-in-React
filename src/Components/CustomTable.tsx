eec748e0eea052ca8ca7a923a99d1a70d6c97ce1
mahnoor <faisalmahnoor74@gmail.com>
2024-01-12 19:16:59 +0500

    pagination added

--- src/Components/CustomTable.tsx
+++ src/Components/CustomTable.tsx
@@ -5,20 +5,39 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TablePagination from '@mui/material/TablePagination';
 import { Paper } from '@mui/material';
 import { Typography } from '@mui/material';
 import { TableProps } from './ItemInterface';
 
 const CustomTable: React.FC<TableProps> = ({ data, headers }) => {
+
+  const [page, setPage] = React.useState(0);
+  
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
   return (
     <React.Fragment>
+      <TablePagination
+      component="div"
+      count={50}
+      page={page}
+      onPageChange={handleChangePage}
+      rowsPerPage={10}
+      rowsPerPageOptions={[10]}
+    />
       <Typography variant="h2"
         sx={{ textAlign: 'center', marginTop: '30px', fontFamily: 'poppins' }}>
         Materialize Table
       </Typography>
       <TableContainer component={Paper}
         sx={{ display: 'flex', justifyContent: 'center', width: '850px', margin: 'auto', padding: "30px" }}>
-        <Table aria-aria-label='simple table'>
+        <Table>
           <TableHead>
             <TableRow
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
@@ -30,8 +49,9 @@ const CustomTable: React.FC<TableProps> = ({ data, headers }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.map((item) => {
-              console.log('Item ID:', item.id);
+            {data
+            .slice(page * 10, page * 10 + 10)
+            .map((item) => {
               return (
                 <TableRow key={item.id}>
                   <TableCell key={'name' + item.id}>{item.name}</TableCell>
