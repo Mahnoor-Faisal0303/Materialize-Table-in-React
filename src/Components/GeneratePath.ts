9779bc1365b6b13c22ffd2df733c87fbaac7829a
mahnoor <faisalmahnoor74@gmail.com>
2024-01-23 17:06:38 +0500

    data change

--- src/Components/GeneratePath.ts
+++ src/Components/GeneratePath.ts
@@ -1,12 +1,11 @@
 import APP_ROUTES from "../Constant/Routes";
 
-export const generatePath =<T>(_url: APP_ROUTES | undefined,  obj:T): string=>{
-       let generatedPath = _url || ""; 
-       for (const i in obj) {
+export const generatePath = <T>(_url: APP_ROUTES | undefined, obj: T): string => {
+    let generatedPath = _url || "";
+    for (const i in obj) {
         const originalURL = `:${i}`;
         const updatedURL = obj[i];
-       generatedPath = generatedPath.replace(originalURL, `${updatedURL}`);
+        generatedPath = generatedPath.replace(originalURL, `${updatedURL}`);
     }
     return generatedPath;
-}
-
+};
--- src/Data/Data.ts
+++ src/Data/Data.ts
@@ -1,16 +1,27 @@
 import { Item } from "../Interfaces/ItemInterface";
 
-export const data: Item[] = [
-    { id: 1, name: "Lion", food: "Meat" },
-    { id: 2, name: "Elephant", food: "Vegetation" },
-    { id: 3, name: "Giraffe", food: "Leaves" },
-    { id: 4, name: "Penguin", food: "Fish" },
-    { id: 5, name: "Kangaroo", food: "Grass" },
-    { id: 6, name: "Dolphin", food: "Fish" },
-    { id: 7, name: "Tiger", food: "Meat" },
-    { id: 8, name: "Monkey", food: "Fruits" },
-    { id: 9, name: "Panda", food: "Bamboo" },
-    { id: 10, name: "Koala", food: "Eucalyptus leaves" }
-];
+// export const data: Item[] = [
+//     { id: 1, name: "Lion", food: "Meat" },
+//     { id: 2, name: "Elephant", food: "Vegetation" },
+//     { id: 3, name: "Giraffe", food: "Leaves" },
+//     { id: 4, name: "Penguin", food: "Fish" },
+//     { id: 5, name: "Kangaroo", food: "Grass" },
+//     { id: 6, name: "Dolphin", food: "Fish" },
+//     { id: 7, name: "Tiger", food: "Meat" },
+//     { id: 8, name: "Monkey", food: "Fruits" },
+//     { id: 9, name: "Panda", food: "Bamboo" },
+//     { id: 10, name: "Koala", food: "Eucalyptus leaves" }
+// ];
 
-export const headers = ["ID", "Animal Name", "Foods"];
+// export const headers = ["ID", "Animal Name", "Foods"];
+export const data: Item[] = [{ id: 1, name: "Mr. Smith", date: "12/Jan/2023" },
+{ id: 2, name: "Ms. Johnson", date: "15/Feb/2023" },
+{ id: 3, name: "Dr. Brown", date: "20/Mar/2023" },
+{ id: 4, name: "Mrs. Davis", date: "25/Apr/2023" },
+{ id: 5, name: "Mr. Taylor", date: "30/May/2023" },
+{ id: 6, name: "Miss White", date: "5/Jun/2023" },
+{ id: 7, name: "Prof. Anderson", date: "10/Jul/2023" },
+{ id: 8, name: "Dr. Martinez", date: "15/Aug/2023" },
+{ id: 9, name: "Mr. Wilson", date: "20/Sep/2023" },
+{ id: 10, name: "Mrs. Miller", date: "25/Oct/2023" }];
+export const headers = ["ID", "Name", "Date"];
